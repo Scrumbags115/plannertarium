9fe2098db5cd6630a91b998872f9caebba213d24
Andrew <anlhu@ucsc.edu>
2023-10-23 11:23:10 -0700

    formatting, new file, fixed getter

--- lib/models/database.dart
+++ lib/models/database.dart
@@ -2,19 +2,36 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:planner_app/models/task.dart';
 
 class DatabaseService {
-
   final String uid;
-  DatabaseService({ required this.uid });
+  DatabaseService({required this.uid});
 
   // users collection reference
-  final CollectionReference users = FirebaseFirestore.instance.collection('users');
+  final CollectionReference users =
+      FirebaseFirestore.instance.collection('users');
 
-  getUserTasks(String taskID) {
-      return FirebaseFirestore.instance.collection('users').doc(uid).collection(taskID); // turn this into a map of taskID to task objects?
+  Future<Task> getUserTasks(String taskID) async {
+    try {
+      var taskDocument = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(uid)
+          .collection('tasks')
+          .doc(taskID)
+          .get();
+      return Task.require(
+          name: taskDocument['name'],
+          description: taskDocument['description'],
+          timeDue: taskDocument['due date'],
+          location: taskDocument['location'],
+          color: taskDocument['hex color'],
+          tags: taskDocument['tags'],
+          recurrenceRules: taskDocument['recurrence rules']);
+    } catch (e) {
+      print("Get Failed");
+      return Task(name: "", tags: <String>{});
+    }
   }
 
   Future<void> setUserTasks(String taskID, Task t) async {
-      return await users.doc(uid).collection('tasks').doc(taskID).set(t.toMap());
+    return await users.doc(uid).collection('tasks').doc(taskID).set(t.toMap());
   }
-
 }
--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,48 +1,50 @@
-/// Class to bundle data for a task/event's recurrance
-class Recurrence {
-    bool enabled;
-    num timeStart;
-    num timeEnd;
-    List<bool> dates;
+import 'package:planner_app/common/recurrence.dart';
 
-    Recurrence(this.enabled, this.timeStart, this.timeEnd, this.dates);
-
-    toMap() {
-        return ({
-            'enabled' : enabled,
-            'ends on' : timeEnd,
-            'repeat on days' : dates,
-        });
-    }
-}
-
+/// Class to hold information about a task
 class Task {
-    String name;
-    String description;
-    num timeDue;
-    String location;
-    String color;
-    Set<String> tags=<String>{};
-    Recurrence? recurrenceRules;
-    final num timeCreated = DateTime.now().millisecondsSinceEpoch/1000;
-    num timeModified=0;
+  String name;
+  String description;
+  num timeDue;
+  String location;
+  String color;
+  Set<String> tags = <String>{};
+  Recurrence? recurrenceRules;
+  final num timeCreated = DateTime.now().millisecondsSinceEpoch / 1000;
+  num timeModified = 0;
 
-    Task({required this.name, this.description="", this.timeDue=0, this.location="", this.color="#919191", required this.tags, this.recurrenceRules}) {
-        timeModified=timeCreated;
-    }
-    
-    toMap() {
-      return ({
-          'date created' : timeCreated,
-          'date modified' : timeModified,
-          'description' : description,
-          'due date' : timeDue,
-          'hex color' : color,
-          'location' : location,
-          'recurrence rules' : recurrenceRules?.toMap() ?? Recurrence(false, 0, 0, []).toMap(),
-          'tags' : tags.toList(),
-          'task name' : name
-          }
-      );
-    }
+  Task(
+      {required this.name,
+      this.description = "",
+      this.timeDue = 0,
+      this.location = "",
+      this.color = "#919191",
+      required this.tags,
+      this.recurrenceRules}) {
+    timeModified = timeCreated;
+  }
+  Task.require(
+      {required this.name,
+      required this.description,
+      required this.timeDue,
+      required this.location,
+      required this.color,
+      required this.tags,
+      required this.recurrenceRules}) {
+    timeModified = timeCreated;
+  }
+
+  toMap() {
+    return ({
+      'date created': timeCreated,
+      'date modified': timeModified,
+      'description': description,
+      'due date': timeDue,
+      'hex color': color,
+      'location': location,
+      'recurrence rules':
+          recurrenceRules?.toMap() ?? Recurrence(false, 0, 0, []).toMap(),
+      'tags': tags.toList(),
+      'task name': name
+    });
+  }
 }
