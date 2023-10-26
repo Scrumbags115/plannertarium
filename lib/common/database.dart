b6858f2199e16505974e7c1df92e2b13145b8df0
stxue1 <stxue@ucsc.edu>
2023-10-25 21:37:23 -0700

    Add task stuff manually

--- lib/common/database.dart
+++ lib/common/database.dart
@@ -196,4 +196,30 @@ class DatabaseService {
       return false;
     }
   }
+
+  Future<Task> getUserTasks(String taskID) async {
+    try {
+      var taskDocument = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(uid)
+          .collection('tasks')
+          .doc(taskID)
+          .get();
+      return Task.require(
+          name: taskDocument['name'],
+          description: taskDocument['description'],
+          timeDue: taskDocument['due date'],
+          location: taskDocument['location'],
+          color: taskDocument['hex color'],
+          tags: taskDocument['tags'],
+          recurrenceRules: taskDocument['recurrence rules']);
+    } catch (e) {
+      print("Get Failed");
+      return Task(name: "", tags: <String>{});
+    }
+  }
+
+  Future<void> setUserTasks(String taskID, Task t) async {
+    return await users.doc(uid).collection('tasks').doc(taskID).set(t.toMap());
+  }
 }
