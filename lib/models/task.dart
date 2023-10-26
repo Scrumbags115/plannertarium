891230ba816e79819344cdcbc727927cdc2d242e
anlhu <130195350+anlhu@users.noreply.github.com>
2023-10-25 21:39:52 -0700

    Andrew task support redo (#3)
    * adding task lib stuff, database
    
    * changed path planner_app to planner
    
    * some things currently workng on
    
    * Basic event support, similar to task support
    
    * Update task.dart with datetime
    
    * no millisecs
    
    * recurrence rules toMap to null
    
    * Move database.dart
    
    * SOmehow the main got disconnected I'm oging to do thsi manually to save
    time
    
    * Add task stuff manually
    
    * Formatting and remove duplicate
    
    ---------
    
    Co-authored-by: stxue1 <stxue@ucsc.edu>

---
+++ lib/models/task.dart
@@ -0,0 +1,52 @@
+import 'package:planner/common/recurrence.dart';
+
+/// Class to hold information about a task
+class Task {
+  String name;
+  String description;
+  DateTime? timeDue;
+  String location;
+  String color;
+  Set<String> tags;
+  Recurrence? recurrenceRules;
+  final DateTime? timeCreated = DateTime.now();
+  DateTime? timeModified = 0;
+
+  Task(
+      {required this.name,
+      this.description = "",
+      this.timeDue = 0,
+      this.location = "",
+      this.color = "#919191",
+      required this.tags,
+      this.recurrenceRules}) {
+    timeModified = timeCreated;
+  }
+
+  /// Alternate constructor so VSCode autogenerates all fields
+  Task.require(
+      {required this.name,
+      required this.description,
+      required this.timeDue,
+      required this.location,
+      required this.color,
+      required this.tags,
+      required this.recurrenceRules}) {
+    timeModified = timeCreated;
+  }
+
+  /// returns a mapping with kv pairs corresponding to Firebase's
+  toMap() {
+    return ({
+      'date created': timeCreated,
+      'date modified': timeModified,
+      'description': description,
+      'due date': timeDue,
+      'hex color': color,
+      'location': location,
+      'recurrence rules': recurrenceRules?.toMap(),
+      'tags': tags.toList(),
+      'task name': name
+    });
+  }
+}
