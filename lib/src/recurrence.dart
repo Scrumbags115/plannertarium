2f5196451266ab5e52cacbfbbb0ab1fd0bbdc735
stxue1 <stxue@ucsc.edu>
2023-10-25 20:41:57 -0700

    abstract events for unique and range

--- lib/src/recurrence.dart
+++ lib/src/recurrence.dart
@@ -1,15 +1,16 @@
 /// Class to bundle data for a task/event's recurrance
 class Recurrence {
   bool enabled;
-  num timeStart;
-  num timeEnd;
-  List<bool> dates;
+  DateTime? timeStart;
+  DateTime? timeEnd;
+  List<bool>? dates;
 
   Recurrence(this.enabled, this.timeStart, this.timeEnd, this.dates);
 
   toMap() {
     return ({
       'enabled' : enabled,
+      'starts on' : timeStart,
       'ends on' : timeEnd,
       'repeat on days' : dates,
     });
