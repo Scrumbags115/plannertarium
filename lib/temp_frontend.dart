e1b9da4dfe628c166a44d32f7db6387f808dbf55
Andrew <anlhu@ucsc.edu>
2023-10-29 22:54:33 -0700

    removed extra main from temp_frontend

--- lib/temp_frontend.dart
+++ lib/temp_frontend.dart
@@ -6,20 +6,6 @@ import 'package:planner/models/task.dart';
 Task t = Task(name: "test", tags: {});
 DatabaseService d = DatabaseService(uid: "test_user_1");
 
-void main() async{
-  print("IN MAIN");
-    WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp(
-        options: const FirebaseOptions(
-            apiKey: 'AIzaSyBwR4cKdPaa5c7p0fMLcAgu-VL8w3L3IUs',
-            appId: '1:86325497409:android:85586ccfa7c01ea29cc0c0',
-            messagingSenderId: '86325497409',
-            projectId: 'plannertarium-d1696'),
-    );
-    d.setUserTasks("1", t);
-    runApp(const MyApp());
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
