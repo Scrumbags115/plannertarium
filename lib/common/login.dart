effa75e4caca6167cfcd72f6a9686607fd9ab42e
stxue1 <stxue@ucsc.edu>
2023-10-31 00:45:04 -0700

    oauth go

--- lib/common/login.dart
+++ lib/common/login.dart
@@ -1,7 +1,34 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
+// final FirebaseAuth auth=FirebaseAuth.instance;
+// final FirebaseFirestore firestore=FirebaseFirestore.instance;
+// final User? user=FirebaseAuth.instance.currentUser;
+// bool login=false;
+//
+// @override
+//
+// void initState(){
+//   super.initState();
+//   FirebaseAuth.instance.setPersistence(Persistence.LOCAL);
+//   auth.authStateChanges().listen((user) {
+//     setState(() {
+//       login=user!=null;
+//     });
+//   });
+// }
+
 /// Sign in with Google SSO, taken from documentation
 Future<UserCredential> signInWithGoogle() async {
+  // GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  //
+  // GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+  //
+  // AuthCredential credential = GoogleAuthProvider.credential(
+  //   accessToken: googleAuth?.accessToken,
+  //   idToken: googleAuth?.idToken,
+  // );
+  //
+  // UserCredential userCreds = await FirebaseAuth.instance.signInWithCredential(credential);
   // Create a new provider
   GoogleAuthProvider googleProvider = GoogleAuthProvider();
 
@@ -14,5 +41,5 @@ Future<UserCredential> signInWithGoogle() async {
   return await FirebaseAuth.instance.signInWithPopup(googleProvider);
 
   // Or use signInWithRedirect
-  // return await FirebaseAuth.instance.signInWithRedirect(googleProvider);
+  // return FirebaseAuth.instance.signInWithRedirect(googleProvider);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,33 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:planner/common/database.dart';
 import 'package:planner/models/task.dart';
-import 'package:planner/temp_frontend.dart';
+import 'package:planner/temp_frontend2.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'common/login.dart';
 
 Task t = Task(name: "test", tags: {});
 DatabaseService d = DatabaseService(uid: "test_user_1");
 
-void main() async{
+void main() async {
   print("IN MAIN");
-    WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp(
-        options: const FirebaseOptions(
-            apiKey: 'AIzaSyBwR4cKdPaa5c7p0fMLcAgu-VL8w3L3IUs',
-            appId: '1:86325497409:android:85586ccfa7c01ea29cc0c0',
-            messagingSenderId: '86325497409',
-            projectId: 'plannertarium-d1696'),
-    );
-    d.setUserTasks("1", t);
-    runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyCZGLE7nkKMlGA5zuxHM0kSACM066Mj8Ao",
+        authDomain: "plannertarium-d1696.firebaseapp.com",
+        projectId: "plannertarium-d1696",
+        storageBucket: "plannertarium-d1696.appspot.com",
+        messagingSenderId: "86325497409",
+        appId: "1:86325497409:web:98f01f217afeb0779cc0c0",
+        measurementId: "G-HM91TY6988"),
+  );
+  d.setUserTasks("1", t);
+  runApp(MyApp());
+
+  await signInWithGoogle();
+
+  User? u = FirebaseAuth.instance.currentUser;
+  print(u);
+  print("weeee");
 }
