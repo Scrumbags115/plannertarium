13d3b6def233b186ffe2ad5ba685f6be1904d1f6
Liam Xu <liamusxu@gmail.com>
2023-10-31 01:00:23 -0700

    small update

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:planner/common/database.dart';
 import 'package:planner/models/task.dart';
 import 'package:planner/temp_frontend.dart';
+import 'package:planner/view/weekView.dart';
+import 'package:get/get.dart';
 
 Task t = Task(name: "test", tags: {});
 DatabaseService d = DatabaseService(uid: "test_user_1");
 
-void main() async{
+void main() async {
   print("IN MAIN");
-    WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp(
-        options: const FirebaseOptions(
-            apiKey: 'AIzaSyBwR4cKdPaa5c7p0fMLcAgu-VL8w3L3IUs',
-            appId: '1:86325497409:android:85586ccfa7c01ea29cc0c0',
-            messagingSenderId: '86325497409',
-            projectId: 'plannertarium-d1696'),
-    );
-    d.setUserTasks("1", t);
-    runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: 'AIzaSyBwR4cKdPaa5c7p0fMLcAgu-VL8w3L3IUs',
+        appId: '1:86325497409:android:85586ccfa7c01ea29cc0c0',
+        messagingSenderId: '86325497409',
+        projectId: 'plannertarium-d1696'),
+  );
+  d.setUserTasks("1", t);
+  runApp(const MyApp());
+  //Uncomment below line and comment out above line to see weekView + dayView UI
+  //runApp(GetMaterialApp(home: weekView()));
 }
