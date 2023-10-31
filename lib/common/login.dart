fc10a9efbb6b02dc43d9f5f1e2900388949e8b85
Baron-Paelen <50848243+Baron-Paelen@users.noreply.github.com>
2023-10-31 01:22:24 -0700

    android google login working. web login is commented out in login.dart

--- lib/common/login.dart
+++ lib/common/login.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 // final FirebaseAuth auth=FirebaseAuth.instance;
 // final FirebaseFirestore firestore=FirebaseFirestore.instance;
@@ -18,28 +19,46 @@ import 'package:firebase_auth/firebase_auth.dart';
 // }
 
 /// Sign in with Google SSO, taken from documentation
-Future<UserCredential> signInWithGoogle() async {
-  // GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-  //
-  // GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-  //
-  // AuthCredential credential = GoogleAuthProvider.credential(
-  //   accessToken: googleAuth?.accessToken,
-  //   idToken: googleAuth?.idToken,
-  // );
-  //
-  // UserCredential userCreds = await FirebaseAuth.instance.signInWithCredential(credential);
-  // Create a new provider
-  GoogleAuthProvider googleProvider = GoogleAuthProvider();
+// Future<UserCredential> signInWithGoogle() async {
+// // GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+// //
+// // GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+// //
+// // AuthCredential credential = GoogleAuthProvider.credential(
+// //   accessToken: googleAuth?.accessToken,
+// //   idToken: googleAuth?.idToken,
+// // );
+// //
+// // UserCredential userCreds = await FirebaseAuth.instance.signInWithCredential(credential);
+// // Create a new provider
+// GoogleAuthProvider googleProvider = GoogleAuthProvider();
 
-  // googleProvider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-  googleProvider.setCustomParameters({
-    'login_hint': 'user@example.com'
-  });
+// // googleProvider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+// googleProvider.setCustomParameters({
+//   'login_hint': 'user@example.com'
+// });
+
+// // Once signed in, return the UserCredential
+// return await FirebaseAuth.instance.signInWithPopup(googleProvider);
+
+// // Or use signInWithRedirect
+// // return FirebaseAuth.instance.signInWithRedirect(googleProvider);
+// }
+
+Future<UserCredential> signInWithGoogle() async {
+  // Trigger the authentication flow
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+  // Obtain the auth details from the request
+  final GoogleSignInAuthentication? googleAuth =
+      await googleUser?.authentication;
+
+  // Create a new credential
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth?.accessToken,
+    idToken: googleAuth?.idToken,
+  );
 
   // Once signed in, return the UserCredential
-  return await FirebaseAuth.instance.signInWithPopup(googleProvider);
-
-  // Or use signInWithRedirect
-  // return FirebaseAuth.instance.signInWithRedirect(googleProvider);
+  return await FirebaseAuth.instance.signInWithCredential(credential);
 }
