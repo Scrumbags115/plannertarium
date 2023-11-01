1ae50c6dd95dc2a6487079794733cbe382bfcf86
Andrew <anlhu@ucsc.edu>
2023-11-01 10:04:00 -0700

    adds new setters and getters for new fields

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -4,6 +4,9 @@ import 'package:planner/common/recurrence.dart';
 class Task {
   String name;
   String description;
+  bool completed;
+  DateTime? timeCurrent;
+  DateTime? timeStart;
   DateTime? timeDue;
   String location;
   String color;
@@ -17,6 +20,8 @@ class Task {
   Task(
       {required this.name,
       this.description = "",
+      this.completed = false,
+      this.timeStart,
       this.timeDue,
       this.location = "",
       this.color = "#919191",
@@ -25,59 +30,97 @@ class Task {
       this.timeCreated}) {
     timeCreated = timeCreated ?? DateTime.now();
     timeModified = DateTime.now();
+    timeCurrent = timeStart;
   }
 
   set Name(String newName) {
     timeModified = DateTime.now();
     name = newName;
   }
+
   String get Name => name;
 
   set Description(String newDescription) {
     timeModified = DateTime.now();
     description = newDescription;
   }
+
   String get Description => description;
 
-  set TimeDue(newTimeDue) { // Can't force DateTime type because it can be null
+  set Completed(bool newCompleted) {
+    timeModified = DateTime.now();
+    completed = newCompleted;
+  }
+
+  bool get Completed => completed;
+
+  set TimeCurrent(newTimeCurrent) {
+    // Can't force DateTime type because it can be null
+    timeModified = DateTime.now();
+    timeCurrent = newTimeCurrent;
+  }
+
+  get TimeCurrent => timeCurrent;
+
+  set TimeStart(newTimeStart) {
+    // Can't force DateTime type because it can be null
+    timeModified = DateTime.now();
+    timeStart = newTimeStart;
+  }
+
+  get TimeStart => timeStart;
+
+  set TimeDue(newTimeDue) {
+    // Can't force DateTime type because it can be null
     timeModified = DateTime.now();
     timeDue = newTimeDue;
   }
+
   get TimeDue => timeDue;
 
   set Location(String newLocation) {
     timeModified = DateTime.now();
     location = newLocation;
   }
+
   String get Location => location;
 
   set Color(String newColor) {
     timeModified = DateTime.now();
     location = newColor;
   }
+
   String get Color => color;
 
   set Tags(Set<String> newTags) {
     timeModified = DateTime.now();
     tags = newTags;
   }
+
   Set<String> get Tags => tags;
 
-  set RecurrenceRules(newRecurrence) { // Can't force Recurrence type because it can be null
+  set RecurrenceRules(newRecurrence) {
+    // Can't force Recurrence type because it can be null
     timeModified = DateTime.now();
     recurrenceRules = newRecurrence;
   }
+
   get RecurrenceRules => recurrenceRules;
 
-  get TimeCreated => timeCreated; // Do not want to timeCreated this after the constructor
+  get TimeCreated =>
+      timeCreated; // Do not want to change timeCreated this after the constructor
 
-  get TimeModified => timeModified; // Do not want to change timeModified unless modifying a field
+  get TimeModified =>
+      timeModified; // Do not want to change timeModified unless modifying a field
 
   /// Alternate constructor so VSCode autogenerates all fields
   /// Good for reading from database
   Task.requireFields(
       {required this.name,
       required this.description,
+      required this.completed,
+      required this.timeCurrent,
+      required this.timeStart,
       required this.timeDue,
       required this.location,
       required this.color,
@@ -87,12 +130,15 @@ class Task {
       required this.timeModified});
 
   /// returns a mapping with kv pairs corresponding to Firebase's
-  /// possibly a better getter 
+  /// possibly a better getter
   Map<String, dynamic> toMap({keepClasses = false}) {
     return ({
       'date created': timeCreated,
       'date modified': timeModified,
+      'completed' : completed,
+      'current date' : timeCurrent,
       'description': description,
+      'start date' : timeStart,
       'due date': timeDue,
       'hex color': color,
       'location': location,
