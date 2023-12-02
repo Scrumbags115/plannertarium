0f4413b5a6318840a5951c6daff344f0334d195d
cchong10 <cchong10@ucsc.edu>
2023-12-01 20:37:33 -0800

    working version of tags

--- lib/view/taskCard.dart
+++ lib/view/taskCard.dart
@@ -16,26 +16,7 @@ class TaskCard extends StatefulWidget {
 
 class TaskCardState extends State<TaskCard> {
   DatabaseService db = DatabaseService();
-  late List<Tag> tagList;
-  final StreamController<List<Tag>> tagListStream = StreamController();
-  
-  @override
-  void initState() {
-    super.initState();
-    updateTagList();
-  }
 
-  /// Get the latest list of tags and input it into the streambuilder controller
-  void updateTagList() async {
-    List<Tag> newTagList = [];
-    for (final String tagID in widget.task.tags) {
-      newTagList.add(await db.getTag(tagID));
-    }
-    tagListStream.add(newTagList);
-  }
-
-  ///A DatePicker function to prompt a calendar
-  ///Returns a selectedDate if chosen, defaulted to today if no selectedDate
   Future<DateTime?> datePicker() async {
     DateTime todayDate = DateTime.now();
     DateTime? selectedDate = await showDatePicker(
@@ -46,13 +27,11 @@ class TaskCardState extends State<TaskCard> {
     );
 
     if (selectedDate != null) {
-      return todayDate;
+      return selectedDate;
     }
     return selectedDate;
   }
 
-  ///A function to check if task is due today
-  ///Returns a boolean true if there is a dueDate && equals to dueDate && task is not completed
   bool isTaskDueToday() {
     DateTime today = DateTime.now();
     DateTime? dueDate = widget.task.timeDue;
@@ -64,8 +43,6 @@ class TaskCardState extends State<TaskCard> {
         !isTaskCompleted());
   }
 
-  ///A function to check if task is completed
-  ///Returns a boolean true if task is completed
   bool isTaskCompleted() {
     bool completed = widget.task.completed;
     return completed;
@@ -77,30 +54,25 @@ class TaskCardState extends State<TaskCard> {
       key: UniqueKey(),
       onDismissed: (direction) async {
         if (direction == DismissDirection.startToEnd) {
-          ///Gesture to delay task to next day
           setState(() {
             widget.task.moveToNextDay();
             db.setTask(widget.task);
           });
         } else if (direction == DismissDirection.endToStart) {
-          ///Gesture to delete task and prompts a dialog box for confirmation
           showDialog(
             context: context,
             builder: (context) {
               return AlertDialog(
                 title: const Text('Confirm Deletion'),
-                content:
-                    const Text('Are you sure you want to delete this task?'),
+                content: const Text('Are you sure you want to delete this task?'),
                 actions: [
                   TextButton(
-                    ///Cancel Button
                     onPressed: () {
                       Navigator.of(context).pop();
                     },
                     child: const Text('Cancel'),
                   ),
                   TextButton(
-                    ///Delete Button
                     onPressed: () {
                       db.deleteTask(widget.task);
                       Navigator.of(context).pop();
@@ -114,7 +86,6 @@ class TaskCardState extends State<TaskCard> {
         }
       },
       background: Container(
-        ///Specification for gestures for delay tasks
         color: const Color.fromARGB(255, 255, 153, 0),
         alignment: Alignment.centerLeft,
         child: const Icon(
@@ -123,7 +94,6 @@ class TaskCardState extends State<TaskCard> {
         ),
       ),
       secondaryBackground: Container(
-        ///Specification for gestures for delete task
         color: Colors.red,
         alignment: Alignment.centerRight,
         child: const Icon(
@@ -139,66 +109,52 @@ class TaskCardState extends State<TaskCard> {
         color: isTaskDueToday() ? Colors.white : Colors.grey.withOpacity(0.5),
         margin: const EdgeInsets.fromLTRB(10, 5, 10, 5),
         child: InkWell(
-
-            ///Inkwell to prompt details of task
-            onTap: () {
-              showDetailPopup(context);
-            },
-            child: ListTile(
-              leading: InkWell(
-                onTap: () {
-                  setState(() {
-                    widget.task.completed = !widget.task.completed;
-                    db.setTask(widget.task);
-                  });
-                },
-                child: CircleAvatar(
-                  backgroundColor:
-                      isTaskCompleted() ? Colors.green : Colors.blue,
-                  child: isTaskCompleted()
-                      ? const Icon(Icons.check, color: Colors.white)
-                      : const Icon(Icons.circle, color: Colors.blue),
-                ),
+          onTap: () {
+            showDetailPopup(context);
+          },
+          child: ListTile(
+            leading: InkWell(
+              onTap: () {
+                setState(() {
+                  widget.task.completed = !widget.task.completed;
+                  db.setTask(widget.task);
+                });
+              },
+              child: CircleAvatar(
+                backgroundColor: isTaskCompleted() ? Colors.green : Colors.blue,
+                child: isTaskCompleted()
+                    ? const Icon(Icons.check, color: Colors.white)
+                    : const Icon(Icons.circle, color: Colors.blue),
               ),
-              title: Text(
-                widget.task.name,
-                style: TextStyle(
-                  decoration:
-                      isTaskCompleted() ? TextDecoration.lineThrough : null,
-                ),
+            ),
+            title: Text(
+              widget.task.name,
+              style: TextStyle(
+                decoration:
+                    isTaskCompleted() ? TextDecoration.lineThrough : null,
               ),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(widget.task.description),
-                  StreamBuilder<List<Tag>>(
-                      stream: tagListStream.stream,
-                      builder: (BuildContext context,
-                          AsyncSnapshot<List<Tag>> snapshot) {
-                        if (snapshot.connectionState != ConnectionState.done) {
-                          // setState(() {});
-                          // not sure what to do here
-                        }
-                        if (snapshot.hasData) {
-                          List<Chip> chipList = [];
-                          for (final Tag tag in (snapshot.data!)) {
-                            Chip tagChip = Chip(
-                                label: Text(tag.name),
-                                backgroundColor: Color(int.parse(tag.color)));
-                            chipList.add(tagChip);
-                          }
-                          return Wrap(spacing: 8.0, children: chipList);
-                        }
-                        return const Text('Loading...');
-                      }),
-                ],
-              ),
-            )),
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(widget.task.description),
+                if (widget.task.tags.isNotEmpty)
+                  Wrap(
+                    spacing: 8.0,
+                    children: widget.task.tags.map((tag) {
+                      return Chip(
+                        label: Text(tag),
+                      );
+                    }).toList(),
+                  ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 
-  ///A void function to prompt for details of a task
   void showDetailPopup(BuildContext context) {
     String formattedDate = widget.task.timeDue != null
         ? DateFormat('yyyy-MM-dd').format(widget.task.timeDue!)
@@ -243,67 +199,21 @@ class TaskCardState extends State<TaskCard> {
     );
   }
 
-  Future<Tag?> showExistingTag() async {
-    Completer<Tag?> completer = Completer<Tag?>();
-    List<Tag> userTags = await db.getAllTags();
-    print(userTags);
-    void showTagDialog(BuildContext dialogContext) {
-      showDialog(
-        context: dialogContext,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('Select Existing Tag'),
-            content: SingleChildScrollView(
-              child: SizedBox(
-                height: (MediaQuery.of(dialogContext).size.height * 0.5),
-                width: (MediaQuery.of(dialogContext).size.width * 0.8),
-                child: ListView.builder(
-                  itemCount: userTags.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(userTags[index].name),
-                      onTap: () {
-                        Navigator.of(context).pop();
-                        completer.complete(userTags[index]);
-                      },
-                    );
-                  },
-                ),
-              ),
-            ),
-            actions: <Widget>[
-              TextButton(
-                child: const Text('Cancel'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  completer.complete(null);
-                },
-              ),
-            ],
-          );
-        },
-      );
-    }
+  Future<List<Tag>> showTagSelectionDialog(BuildContext context) async {
+    List<Tag> selectedTags = [];
 
-    // Call the function with the current BuildContext
-    showTagDialog(context);
-
-    return completer.future;
-  }
-
-  Future<Tag?> showTagSelectionDialog(BuildContext context) async {
     TextEditingController nameController = TextEditingController();
-    Color selectedColor = Colors.blue; // Default color
+    Color selectedColor = Colors.blue;
     Color pickerColor = Color(0xff443a49);
 
     void changeColor(Color color) {
       setState(() {
         pickerColor = color;
-        selectedColor = color; // Update the selectedColor as well
+        selectedColor = color;
       });
     }
 
-    return showDialog(
+    await showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
@@ -320,8 +230,8 @@ class TaskCardState extends State<TaskCard> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    const Text('Tag Color:'),
-                    SizedBox(
+                    Text('Tag Color:'),
+                    Container(
                       width: 200,
                       child: ColorPicker(
                         pickerColor: pickerColor,
@@ -336,16 +246,18 @@ class TaskCardState extends State<TaskCard> {
           actions: [
             TextButton(
               onPressed: () {
-                Navigator.pop(context);
+                Navigator.pop(context, selectedTags);
               },
               child: const Text('Cancel'),
             ),
             TextButton(
               onPressed: () {
                 Tag selectedTag = Tag(
-                    name: nameController.text, color: selectedColor.toString());
-                print(selectedColor.toString());
-                Navigator.pop(context, selectedTag);
+                  name: nameController.text,
+                  color: selectedColor.toString(),
+                );
+                selectedTags.add(selectedTag);
+                nameController.clear();
               },
               child: const Text('Add'),
             ),
@@ -353,6 +265,8 @@ class TaskCardState extends State<TaskCard> {
         );
       },
     );
+
+    return selectedTags;
   }
 
   Future<Task?> showEditPopup(BuildContext context) async {
@@ -365,7 +279,8 @@ class TaskCardState extends State<TaskCard> {
     DateTime? startTime = DateTime.now();
     Completer<Task?> completer = Completer<Task?>();
 
-    List<Tag> enteredTags = []; // Use a list to store multiple tags
+    List<Tag> enteredTags = [];
+    Tag? selectedTag;
 
     showDialog(
       context: context,
@@ -383,66 +298,57 @@ class TaskCardState extends State<TaskCard> {
                     children: <Widget>[
                       TextField(
                         controller: nameController,
-                        decoration:
-                            const InputDecoration(labelText: 'Task Name'),
+                        decoration: const InputDecoration(labelText: 'Task Name'),
                       ),
                       TextField(
                         controller: descriptionController,
-                        decoration:
-                            const InputDecoration(labelText: 'Description'),
+                        decoration: const InputDecoration(labelText: 'Description'),
                       ),
                       TextField(
                         controller: locationController,
-                        decoration:
-                            const InputDecoration(labelText: 'Location'),
+                        decoration: const InputDecoration(labelText: 'Location'),
                       ),
-                      TextField(
-                        controller: tagController,
-                        decoration: const InputDecoration(
-                          labelText: 'Tag',
-                          suffixIcon: Icon(Icons.keyboard_arrow_down),
-                        ),
-                        onTap: () async {
-                          Tag? result = await showTagSelectionDialog(context);
-                          if (result != null) {
-                            setState(() {
-                              enteredTags.add(result);
-                            });
-                          }
-                        },
-                      ),
-                      SizedBox(
-                        height: 60,
+                      Container(
+                        height: 40,
                         child: ListView.builder(
                           scrollDirection: Axis.horizontal,
                           itemCount: enteredTags.length,
                           itemBuilder: (context, index) {
-                            return GestureDetector(
-                              onTap: () {
-                                setState(() {
-                                  enteredTags.removeAt(index);
-                                });
-                              },
-                              child: Container(
-                                height: 50,
-                                margin: const EdgeInsets.symmetric(
-                                    horizontal: 4, vertical: 10),
-                                padding: const EdgeInsets.all(5),
-                                decoration: BoxDecoration(
-                                  color: Colors.grey,
-                                  borderRadius: BorderRadius.circular(15),
-                                ),
-                                child: Row(
-                                  children: [
-                                    Text(
-                                      enteredTags[index].name,
-                                    ),
-                                  ],
-                                ),
+                            return Container(
+                              margin: EdgeInsets.symmetric(horizontal: 4),
+                              padding: EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: Row(
+                                children: [
+                                  Text(enteredTags[index].name),
+                                  IconButton(
+                                    icon: Icon(Icons.clear),
+                                    onPressed: () {
+                                      setState(() {
+                                        enteredTags.removeAt(index);
+                                      });
+                                    },
+                                  ),
+                                ],
                               ),
                             );
                           },
                         ),
+                      ),
+                      TextField(
+                        controller: tagController,
+                        decoration: const InputDecoration(labelText: 'Tag'),
+                        onTap: () async {
+                          List<Tag> result = await showTagSelectionDialog(context);
+                          if (result.isNotEmpty) {
+                            setState(() {
+                              enteredTags.addAll(result);
+                            });
+                          }
+                        },
                       ),
                       Row(
                         children: [
@@ -471,9 +377,7 @@ class TaskCardState extends State<TaskCard> {
                             },
                           ),
                           const SizedBox(width: 8),
-                          const Text(
-                            'Due Time',
-                          ),
+                          const Text('Due Time'),
                         ],
                       ),
                     ],
@@ -491,14 +395,13 @@ class TaskCardState extends State<TaskCard> {
                 TextButton(
                   child: const Text('Submit'),
                   onPressed: () {
+                    if (enteredTags.isNotEmpty) {
+                      widget.task.tags = enteredTags.map((tag) => tag.name).toList();
+                    }
                     String name = nameController.text;
                     String description = descriptionController.text;
                     String location = locationController.text;
 
-                    List<String> tags =
-                        enteredTags.map((tag) => tag.name).toList();
-
-                    widget.task.tags = tags;
                     widget.task.name = name;
                     widget.task.description = description;
                     widget.task.location = location;
