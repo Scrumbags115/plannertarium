fd872d604371811ff16c1bf11e361522f5890965
stxue1 <stxue@ucsc.edu>
2023-12-03 22:35:11 -0800

    remove unused

--- lib/view/loginView.dart
+++ lib/view/loginView.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:planner/common/database.dart';
 import 'package:flutter/material.dart';
